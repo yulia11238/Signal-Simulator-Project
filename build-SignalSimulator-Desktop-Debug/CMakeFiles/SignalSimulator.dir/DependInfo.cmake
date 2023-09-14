df833a1a96238b2b3d0bf4421b4dcc481a8fdcd1
ylng1917 <karbivskayulia@gmail.com>
2023-09-14 16:33:08 -0500

    Reorganized project structure with src and include directories.

---
+++ build-SignalSimulator-Desktop-Debug/CMakeFiles/SignalSimulator.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/yulia/Project/build-SignalSimulator-Desktop-Debug/SignalSimulator_autogen/mocs_compilation.cpp" "CMakeFiles/SignalSimulator.dir/SignalSimulator_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/SignalSimulator.dir/SignalSimulator_autogen/mocs_compilation.cpp.o.d"
+  "/home/yulia/Project/SignalSimulator/main.cpp" "CMakeFiles/SignalSimulator.dir/main.cpp.o" "gcc" "CMakeFiles/SignalSimulator.dir/main.cpp.o.d"
+  "/home/yulia/Project/SignalSimulator/signalgenerator.cpp" "CMakeFiles/SignalSimulator.dir/signalgenerator.cpp.o" "gcc" "CMakeFiles/SignalSimulator.dir/signalgenerator.cpp.o.d"
+  "/home/yulia/Project/SignalSimulator/widget.cpp" "CMakeFiles/SignalSimulator.dir/widget.cpp.o" "gcc" "CMakeFiles/SignalSimulator.dir/widget.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
