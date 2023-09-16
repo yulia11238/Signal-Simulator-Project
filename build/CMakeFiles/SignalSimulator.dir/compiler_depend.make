29ccf2a1c3e4bc4e5b3f3d2ccd48efa96570ea8b
ylng1917 <karbivskayulia@gmail.com>
2023-09-16 18:43:38 -0500

    Moved widget.ui to /src

---
+++ build/CMakeFiles/SignalSimulator.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for SignalSimulator.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/SignalSimulator_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SignalSimulator_autogen"
+  "SignalSimulator_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/SignalSimulator_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
